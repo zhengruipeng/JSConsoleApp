14cbe8e56f10d0b2266356df4b7712475d35cdd4
miki <37892435+zhengruipeng@users.noreply.github.com>
2023-05-12 17:00:36 +0800

    Add files via upload

--- Console.js
+++ Console.js
@@ -13,6 +13,7 @@ let Console = class extends Object {
             width:100vw;
             overflow-y:scroll;
         }
+
         #console-panel>p{
             word-break:break-all;
             font-family: Source Code Pro;
@@ -30,7 +31,7 @@ let Console = class extends Object {
         }
         /*the style of editable p element */
         #console-panel>p.editable{
-            user-select: none;
+            /*user-select: none;*/
             cursor:default;
         }
         @keyframes inputPointer {
@@ -55,12 +56,105 @@ let Console = class extends Object {
             animation-iteration-count: infinite;
         }
     `;
+    //Inline Style Sheet
+    #consoleInlineStyleSheet = `
+        #console-panel{
+            background-color: #000;
+            color:#fff;
+            index: 10;
+            position:fixed;
+            top:0;
+            right:0;
+            height:100%;
+            width:30%;
+            overflow-y:scroll;
+            transition:.3s;
+        }
+        #console-panel-cancel-btn{
+            position:fixed;
+            top:0;
+            right:30%;
+            height:100%;
+            width:1rem;
+            border:none;
+            transition:.3s;
+        }
+        #console-panel>p{
+            word-break:break-all;
+            font-family: Source Code Pro;
+            font-size:1.2rem;
+            padding:.2em 1em;
+            color: transparent;
+            text-shadow: 0 0 0 #fff;
+            outline:none;
+        }
+        #console-panel *{
+            word-break:break-all;
+            font-family: Source Code Pro;
+            font-size:1.2rem;
+            padding:.2em 1em;
+        }
+        /*the style of editable p element */
+        #console-panel>p.editable{
+            /*user-select: none;*/
+            cursor:default;
+        }
+        @keyframes inputPointer {
+            from {
+                border-bottom-style: solid;
+            }
+            to {
+                border-bottom-style: none;
+            }
+        }
+
+        #console-panel > p.editable::after {
+            content: " ";
+            display:inline-block;
+            width:10px;
+            margin-left:5px;
+            border-bottom: #fff 7px solid;
+            animation-name: inputPointer;
+            animation-duration: .2s;
+            animation-timing-function: linear;
+            animation-direction: alternate;
+            animation-iteration-count: infinite;
+        }
+        #console-panel.cancel{
+            right:-30%;
+        }
+        #console-panel-cancel-btn.cancel{
+            right:0%;
+        }
+    `;
 
     //HTMLElement, the main element of control panel
     #consolePanel = null;
+    //the cancel button of panel, only inline panel use it
+    #consolePanelCancelBtn = null;
 
     //the editing p element
     #editingInput = null;
+
+    autoFinish = true;
+    //the timer of operate duration
+    timer = null;
+
+    //clear the past timer and bind a new timer
+    updateTimer() {
+        if (!this.autoFinish) return false;
+
+        if (this.timer) clearTimeout(this.timer);
+        this.timer = setTimeout(this.end.bind(this), 10);
+    }
+
+    //cancel timer
+    cancelTimer() {
+        if (!this.autoFinish) return false;
+
+        clearTimeout(this.timer);
+        this.timer = null;
+    }
 
     /*
     * @name: setEditingInput;
@@ -75,9 +169,11 @@ let Console = class extends Object {
         if (htmlPElement) {
             htmlPElement.contentEditable = true;
             htmlPElement.classList.add("editable");
+            this.cancelTimer();
         } else {
             this.#editingInput.contentEditable = false;
             this.#editingInput.classList.remove("editable");
+            this.updateTimer();
         }
 
 
@@ -117,7 +213,7 @@ let Console = class extends Object {
             });
 
             that.#editingInput?.addEventListener("blur", function (ev) {
-                this.focus()
+                // this.focus()
             });
 
             that.#editingInput.focus();
@@ -151,6 +247,46 @@ let Console = class extends Object {
             that.#getEditingInput().focus();
         });
 
+        this.updateTimer();
+
+        return consolePanel;
+    }
+
+    //Preparatory work
+    //1. Create the console and set properties
+    //2. Set the style sheet
+    //3. Add it to the page
+    renderAsConsoleInline(/*HTMLElement*/ container = document.body) {
+        let consolePanel = document.createElement("div");
+        consolePanel.id = "console-panel";
+
+        let consolePanelCancelBtn = document.createElement("button");
+        consolePanelCancelBtn.id = "console-panel-cancel-btn";
+        consolePanelCancelBtn.innerText = "||";
+
+        container.appendChild(consolePanelCancelBtn);
+        container.appendChild(consolePanel);
+
+        this.#consolePanelCancelBtn = consolePanelCancelBtn;
+        this.#consolePanel = consolePanel;
+
+        let stylesheet = new CSSStyleSheet();
+        stylesheet.replaceSync(this.#consoleInlineStyleSheet);
+
+        document.adoptedStyleSheets = [stylesheet]
+
+        let that = this;
+        this.#consolePanel.addEventListener("click", function () {
+            if (!that.#getEditingInput()) return false;
+            that.#getEditingInput().focus();
+        });
+        this.#consolePanelCancelBtn.addEventListener("click", function () {
+            that.#consolePanel.classList.toggle("cancel");
+            this.classList.toggle("cancel");
+        });
+
+        this.autoFinish = false;
+        this.updateTimer();
         return consolePanel;
     }
 
@@ -172,11 +308,14 @@ let Console = class extends Object {
         let that = this;
         msgs.forEach(msg => {
             that.#initItem(msg);
-        })
+        });
+
+        this.updateTimer();
     }
 
     end(msg = "The program has ended....") {
         this.output(msg);
+        this.cancelTimer();
     }
 
     /*
@@ -188,6 +327,10 @@ let Console = class extends Object {
     *  divided into an array of strings, and returned
     * */
     async input() {
+        if (this.#getEditingInput()) {
+            return false;
+        }
+
         let p = this.#initItem("");
 
         await this.#setEditingInput(p);
